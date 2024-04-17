06a71698b024eccded50a328542191d64a01b142
O Sawani <omarsawani@gmail.com>
2024-04-17 12:51:57 +0000

    commit

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -22,10 +22,10 @@ class Task(db.Model):
     #schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
-    task_description = db.Column(db.text, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete='CASCADE'), nullable=False) # once category is deleted all tasks are deleted
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False) # once category is deleted all tasks are deleted
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
